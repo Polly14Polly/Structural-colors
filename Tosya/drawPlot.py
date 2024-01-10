84a04d1d8e42e501d87472f22399cb5b957d4957
survi <survivor228.fish@gmail.com>
2024-01-10 17:08:52 +0300

    фигня всякая

---
+++ Tosya/drawPlot.py
@@ -0,0 +1,17 @@
+import numpy as np
+import math
+import matplotlib.pyplot as G
+
+read = open('CdS 100sprs x 140R.txt', 'r')
+cmd = read.readline()  # прочитал строку
+Xs = []
+Ys = []
+while cmd != "" and cmd != " ":
+    cmd = cmd.replace("\n", "")
+    cmds = cmd.split(" ")
+    Xs.append(float(cmds[0]))
+    Ys.append(float(cmds[1]))
+    cmd = read.readline();  # прочитал строку
+
+G.plot(Xs, Ys)
+G.show()
