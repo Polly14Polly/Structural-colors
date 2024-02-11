705951688ca521480557c3372d7d9dd11ce6c657
anatoliy <anatoliysemenov2006@gmail.com>
2024-02-11 15:34:48 +0300

    full_random_ready_nafig_uraaaa

--- Tosya/Our_CIE/CIE.py
+++ Tosya/Our_CIE/CIE.py
@@ -1,39 +1,58 @@
 import pygame
 from settings import *
 
-name = input("Название >>>")
+def cie_from_spec(file):
+    pass
 
-pygame.init()
+name = input("Название >>>")
 
 screen = pygame.display.set_mode((800, 816))
 
-bg = pygame.image.load("cie_img.jpg")
+bg = pygame.image.load("cie_img.png")
 
-coords = [
-    [0.32, 0.5],
-    [0.12, 0.21],
-    [0.3, 0.4]
-]
+files = []
 
-loop = True
+instruction = input("Введите название файла со спектром или start (для расчёта) >>> ")
 
-while loop:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            loop = False
-            pygame.image.save(screen, f"results/{name}.png")
-            pygame.quit()
+while instruction != "exit":
+    files.append(instruction)
+    instruction = input("Введите название файла со спектром или start (для расчёта) >>> ")
 
-    screen.blit(bg, (0, 0))
+coords = []
 
-    for coord in coords:
-        x = coord[0]
-        y = coord[1]
-        pygame.draw.circle(
-            screen,
-            (0, 0, 0),
-            (OFFSET_X + x*GLOBAL_DELTA_X/LOCAL_DELTA_X, 816 - (OFFSET_Y + y*GLOBAL_DELTA_Y/LOCAL_DELTA_Y)),
-            3
-        )
+n = 1
+for spec in files:
+    cie_coords = cie_from_spec(spec)
+    coords.append([cie_coords[0], cie_coords[1], n, spec])
 
-    pygame.display.update()
+title = []
+for coord in coords:
+    title.append(f"{coord[2]} - {coord[3]}")
+
+pygame.init()
+
+screen.blit(bg, (0, 0))
+
+for coord in coords:
+    x = coord[0]
+    y = coord[1]
+    pygame.draw.circle(
+        screen,
+        (0, 0, 0),
+        (OFFSET_X + x*GLOBAL_DELTA_X/LOCAL_DELTA_X, 816 - (OFFSET_Y + y*GLOBAL_DELTA_Y/LOCAL_DELTA_Y)),
+        3
+    )
+
+    font = pygame.font.SysFont(None, 24)
+    img = font.render(f'{coord[2]}', True, (0, 0, 0))
+    screen.blit(img, (OFFSET_X + x*GLOBAL_DELTA_X/LOCAL_DELTA_X + 8, 816 - (OFFSET_Y + y*GLOBAL_DELTA_Y/LOCAL_DELTA_Y + 8)))
+
+dk = 0
+for t in title:
+    font = pygame.font.SysFont(None, 24)
+    img = font.render(f'{t}', True, (255, 255, 255))
+    screen.blit(img, (500 ,60 + dk))
+    dk += 30
+
+pygame.image.save(screen, f"results/{name}.png")
+pygame.quit()
--- Tosya/full_random/full_random.py
+++ Tosya/full_random/full_random.py
@@ -1,6 +1,7 @@
 import random
 from settings import *
 import os
+import colour
 import pandas
 import pygame as pg
 import matplotlib.pyplot
@@ -11,19 +12,26 @@ import smuthi.layers
 import smuthi.particles
 import smuthi.postprocessing.far_field as ff
 
-name = input("Озаглавьте папку >>> ")
+instructions = open("instructions.txt", "r")
 
-r_screen_width = float(input("Ширина x поля в нанометрах/100 >>> "))
-r_screen_height = float(input("Ширина y поля в нанометрах/100 >>> "))
-mid_radius = float(input("Радиус в нанометрах/100 >>> "))
-screen_width = 1000
-screen_height = 500
-k = screen_width / r_screen_width
-
-os.mkdir(name)
+instructions_lines = instructions.readlines()
 
 
-def structure():
+def structure(instruction):
+    instruction_array = instruction.split(";")
+
+    name = instruction_array[0]
+
+    r_screen_width = float(instruction_array[1])
+    r_screen_height = float(instruction_array[2])
+    mid_radius = float(instruction_array[3])
+    number_of_spheres = int(instruction_array[4])
+    screen_width = 1000
+    screen_height = 500
+    k = screen_width / r_screen_width
+
+    os.mkdir(name)
+
     spheres = []
 
     def full_random(num_of_spheres):
@@ -47,10 +55,6 @@ def structure():
         spheres.remove([0., 0., 0.])
 
     def view():
-        global screen_width
-        global screen_height
-        global k
-
         pg.init()
 
         screen = pg.display.set_mode((screen_width, screen_height))
@@ -95,12 +99,9 @@ def structure():
                     (abs(float(n_array[wave_btw[0]][2])) + abs(delta_2*scale))*1j)
 
     def smuthi_calculation_wave():
-        global r_screen_width
-        global r_screen_height
-
         spectrum = []
 
-        for wave in range(380, 780, 20):
+        for wave in range(380, 800, 20):
             spheres_for_smuthi = []
 
             sphere_ref_ind = get_ref_index(wave, "Si")
@@ -150,9 +151,6 @@ def structure():
         return spectrum
 
     def smuthi_calculation_angle(wave):
-        global r_screen_width
-        global r_screen_height
-
         angle_spectrum = []
 
         angle = 0
@@ -209,25 +207,76 @@ def structure():
 
         return angle_spectrum
 
+    def smuthi_calculation_one_particle():
+        one_particle_spectrum = []
+
+        for wave in range(380, 800, 20):
+            spheres_for_smuthi = []
+
+            sphere_ref_ind = get_ref_index(wave, "Si")
+            layer_ref_ind = get_ref_index(wave, "SiO2")
+            spheres_for_smuthi.append(
+                smuthi.particles.Sphere(
+                    position=[r_screen_width/2*100, r_screen_height/2*100, mid_radius*100],
+                    refractive_index=sphere_ref_ind,
+                    radius=mid_radius*100,
+                    l_max=3
+                )
+            )
+
+            layers = smuthi.layers.LayerSystem(thicknesses=[0, 0], refractive_indices=[layer_ref_ind, 1])
+
+            plane_wave = smuthi.initial_field.PlaneWave(
+                vacuum_wavelength=wave,
+                polar_angle=np.pi,
+                azimuthal_angle=0,
+                polarization=0
+            )
+
+            simulation = smuthi.simulation.Simulation(
+                layer_system=layers,
+                particle_list=spheres_for_smuthi,
+                solver_type='gmres',
+                solver_tolerance=1e-7,
+                initial_field=plane_wave
+            )
+
+            simulation.run()
+
+            scs = ff.total_scattering_cross_section(
+                initial_field=plane_wave,
+                particle_list=spheres_for_smuthi,
+                layer_system=layers
+            )
+
+            norm = r_screen_width*r_screen_height*10000
+
+            scs = scs / norm
+
+            one_particle_spectrum.append([scs, wave])
+
+        return one_particle_spectrum
+
     def sum_with_scale(array, delta):
         sum_ = 0
-        for i in range(0, len(array) - 1, delta):
+        for i in range(0, len(array) - 1, delta//5):
             sum_ += (array[i] + array[i + 1]) / 2
 
         return sum_
 
     def mult_integral(delta, *args):
         mult_fun = []
-        for i in range(0, len(args[0]) - 1, delta):
+        for i in range(0, len(args[0]) - 1, delta//5):
             el = 1
             for fun in args:
                 if args.index(fun) == 1:
+                    print(int(i * 5 / delta), len(args[1]), len(args[0]))
                     el *= (fun[int(i * 5 / delta)] + fun[int(i * 5 / delta) + 1]) / 2
                 else:
                     el *= (fun[i] + fun[i + 1]) / 2
 
             mult_fun.append(el)
-            print(i)
+            # print(i)
 
         return sum_with_scale(mult_fun, delta)
 
@@ -253,10 +302,11 @@ def structure():
 
         X = pre_coordinates[0] / s
         Y = pre_coordinates[1] / s
+        Z = pre_coordinates[2] / s
 
-        return [X, Y]
+        return colour.XYZ_to_xy([X, Y, Z])
 
-    def cie_graph(coordinates):
+    def cie_graph(coordinates, n):
         pg.init()
 
         screen = pg.display.set_mode((800, 816))
@@ -280,10 +330,10 @@ def structure():
         text = font.render(f'{name}', True, (0, 0, 0))
         screen.blit(text, (0, 0))
 
-        pg.image.save(screen, f"{name}/cie.png")
+        pg.image.save(screen, f"{name}/cie{n}.png")
         pg.quit()
 
-    full_random(int(input("Кол-во сфер >>> ")))
+    full_random(number_of_spheres)
     view()
 
     x = []
@@ -302,7 +352,7 @@ def structure():
 
     matplotlib.pyplot.close()
 
-    cie_graph(cie_from_spectrum(x, y))
+    cie_graph(cie_from_spectrum(x, y), 1)
 
     max_wave = x[y.index(max(y))]
 
@@ -320,5 +370,26 @@ def structure():
     matplotlib.pyplot.ylabel("Normalized cross-sections")
     matplotlib.pyplot.savefig(f"{name}/angle_spectrum.png")
 
+    matplotlib.pyplot.close()
 
-structure()
+    one_particle_x = []
+    one_particle_y = []
+
+    one_particle_spec = smuthi_calculation_one_particle()
+
+    for point in one_particle_spec:
+        one_particle_x.append(point[1])
+        one_particle_y.append(point[0])
+
+    matplotlib.pyplot.plot(one_particle_x, one_particle_y)
+    matplotlib.pyplot.xlabel("Wavelength [nm]")
+    matplotlib.pyplot.ylabel("Normalized cross-sections")
+    matplotlib.pyplot.savefig(f"{name}/one_particle_spectrum.png")
+
+    matplotlib.pyplot.close()
+
+    cie_graph(cie_from_spectrum(one_particle_x, one_particle_y), 2)
+
+
+for inst in instructions_lines:
+    structure(inst)
