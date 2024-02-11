aa15c3c8ab2995e1c808fcb6ba9ff42053b098f4
survi <survivor228.fish@gmail.com>
2024-02-11 23:02:01 +0300

    дофига спектров

--- PbIb/gigacode/drawPlot.py
+++ PbIb/gigacode/drawPlot.py
@@ -2,7 +2,7 @@ import numpy as np
 import math
 import matplotlib.pyplot as G
 
-read = open('8plot.txt', 'r')
+read = open('Si path/8plot.txt', 'r')
 cmd = read.readline()  # прочитал строку
 Xs = []
 Ys = []
