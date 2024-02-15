51dc89b0ca95ac93ebe52d9695c206830dd9688c
Olegftsh <shablyaoleg@gmail.com>
2024-02-15 23:22:24 +0300

    text

--- Oleg/1.1.3.2 calculation const r rect net  diff d_length.py
+++ Oleg/1.1.3.2 calculation const r rect net  diff d_length.py
@@ -17,9 +17,103 @@ import smuthi.utility.automatic_parameter_selection
 import random
 
 
+def get_ref_index(wave, material):
+    dependencies = {
+        "SiO2": "SiO2_Gao.txt",
+        "Si": "Si_Vuye_20C.txt"
+    }
+    file = open(dependencies[material], "r")
+    lines = file.readlines()
+    n_array = []
+    for line in lines:
+        while line[0] == " ":
+            line = line[1:]
+        n_array.append(line.split(" "))
+
+    wave_ind = 0
+    while float(n_array[wave_ind][0]) * 1000 < wave:
+        wave_ind += 1
+    wave_btw = [wave_ind - 1, wave_ind]
+    delta_wave_1 = wave - float(n_array[wave_btw[0]][0]) * 1000
+    delta_wave_2 = float(n_array[wave_btw[1]][0]) * 1000 - wave
+    if delta_wave_2 == 0:
+        return float(n_array[wave_btw[1]][1]) + float(n_array[wave_btw[1]][2]) * 1j
+    if delta_wave_1 == 0:
+        return float(n_array[wave_btw[0]][1]) + float(n_array[wave_btw[0]][2]) * 1j
+    else:
+        scale = delta_wave_1 / (delta_wave_2 + delta_wave_1)
+        delta_1 = abs(float(n_array[wave_btw[1]][1]) - float(n_array[wave_btw[0]][1]))
+        delta_2 = abs(float(n_array[wave_btw[1]][2]) - float(n_array[wave_btw[0]][2]))
+        return ((abs(float(n_array[wave_btw[0]][1])) + abs(delta_1 * scale)) +
+                (abs(float(n_array[wave_btw[0]][2])) + abs(delta_2 * scale)) * 1j)
 
 
-                                #начальные условия
+def smuthi_calculation_with_some_angles(spheres):
+    spectrum_with_some_angles = []
+
+    for angle in [np.pi, np.pi * 5 / 6, np.pi * 2 / 3]:
+        waves = []
+        scss = []
+
+        for wave in range(380, 800, 20):
+            spheres_for_smuthi = []
+
+            sphere_ref_ind = get_ref_index(wave, "Si")
+            layer_ref_ind = get_ref_index(wave, "SiO2")
+            for sphere in spheres:
+                print([sphere[0] * 100, sphere[1] * 100, sphere[2] * 100])
+                spheres_for_smuthi.append(
+                    smuthi.particles.Sphere(
+                        position=[sphere[0] * 100, sphere[1] * 100, sphere[2] * 100],
+                        refractive_index=sphere_ref_ind,
+                        radius=sphere[2] * 100,
+                        l_max=3
+                    )
+                )
+
+            layers = smuthi.layers.LayerSystem(thicknesses=[0, 0], refractive_indices=[layer_ref_ind, 1])
+
+            plane_wave = smuthi.initial_field.PlaneWave(
+                vacuum_wavelength=wave,
+                polar_angle=angle,
+                azimuthal_angle=0,
+                polarization=0
+            )
+
+            simulation = smuthi.simulation.Simulation(
+                layer_system=layers,
+                particle_list=spheres_for_smuthi,
+                solver_type='gmres',
+                solver_tolerance=1e-7,
+                initial_field=plane_wave
+            )
+
+            simulation.run()
+
+            scs = ff.total_scattering_cross_section(
+                initial_field=plane_wave,
+                particle_list=spheres_for_smuthi,
+                layer_system=layers
+            )
+            r_screen_width=800
+            r_screen_height=800
+            norm = r_screen_width * r_screen_height * 10000
+
+            scs = scs / norm
+
+            waves.append(wave)
+            scss.append(scs)
+
+        matplotlib.pyplot.plot(waves, scss, label=f"Si, r=100nm, N=4 angle={int(180 * angle / np.pi)}")
+    matplotlib.pyplot.legend()
+    matplotlib.pyplot.xlabel("Wavelength [nm]")
+    matplotlib.pyplot.ylabel("Normalized cross-sections")
+    matplotlib.pyplot.show()
+    matplotlib.pyplot.savefig(f"spectrum_with_different_angles.png")
+
+    matplotlib.pyplot.close()
+
+        #начальные условия
 leftGran = 325                  #минимальная длина волны, нм
 rightGran = 900                 #максимальная длина волны, нм
 shag = 25                   #шаг, с коротым будут ставиться точки(длина волны, нм)
@@ -99,7 +193,7 @@ import random
 leftGran = 380                  #минимальная длина волны, нм
 rightGran = 780
 
-diapasons=[ [350, 525, 2], [530, 600, 2],[600, 650, 2] ,[650, 690, 2], [700, 780, 2]]
+diapasons=[ [380, 780, 10]]
 arr_waves = []
 for i in diapasons:
     for j in range(i[0], i[1], i[2]):
@@ -153,8 +247,8 @@ def p(a):
 
 
 # задаем размеры подложки
-size_x = 1000
-size_y = 1000
+size_x = 1500
+size_y = 1500
 # размер ячейки, в которую будем помещать сферу
 a = 250
 b = 250
@@ -272,4 +366,6 @@ f.close()
 
 G.plot(x, bI) #строю графек
 G.show()
-
+G.close()
+#smuthi_calculation_with_some_angles(arr)
+print(arr)
--- Oleg/2.1 rect_net 1.py
+++ Oleg/2.1 rect_net 1.py
@@ -1,18 +1,3 @@
-from tkinter import *
-# подготовка поля
-from tkinter import *
-window = Tk()
-c = Canvas(window,width=1000, height=1000) # Холст 1000
-c.pack()
-window.title('Структурка') # Заголовок
-
-def circ(x, y, r):
-    x1=x-r
-    x2=x+r
-    y1=y-r
-    y2=y+r
-    c.create_oval(x1, y1, x2, y2)
-
 import random
 def p(a):
     for i in a:
@@ -21,43 +6,42 @@ def p(a):
         print()
 
 # задаем размеры подложки
-size_x=1200
-size_y=1200
+size_x=2400
+size_y=2400
 # размер ячейки, в которую будем помещать сферу
-a = 30
-b = 30
+a = 600
+b = 600
+# радиус
+r = 250
+e_dr = 0.05 # погрешность радиуса 5%
+dr=r*e_dr
 Na=size_x//a
 Nb=size_y//b
-
+N=Na*Nb
 arr=[] # относительные координаты сфер
 for i in range(Na):
     arr.append([])
     for j in range (Nb):
         arr[i].append([])
 
-        r=10
+        r=random.uniform(r-dr, r+dr)
+        x=random.uniform(r+dr+1, a-r-dr-1)
+        y=random.uniform(r+dr+1, b-r-dr-1)
+        z=r
 
-        x=random.randint(r+1, a-r-1)
-        y=random.randint(r+1, b-r-1)
-        arr[i][j].append(x)
-        arr[i][j].append(y)
-        arr[i][j].append(r)
+        arr[i][j].append(round(x+i*a, 2))
+        arr[i][j].append(round(y+j*b, 2))
+        arr[i][j].append(round(z, 2))
+        arr[i][j].append(round(r, 2))
 #p(arr)
 # каждая ячейка массива Na*Nb содержит в себе массив - пару чисел (координаты центра ОТНОСИТЕЛЬНО ЛЕВОГО НИЖНЕГО УГЛА) и радиус
 
 arr1=arr # абсолютные координаты
 for i in range(Na):
     for j in range(Nb):
-        arr1[i][j][0]=arr[i][j][0]+a*i
-        arr1[i][j][1] = arr[i][j][1]+b*j
-
+        arr1[i][j][0] = round(arr[i][j][0]+a*i, 2)
+        arr1[i][j][1] = round(arr[i][j][1]+b*j, 2)
+print(N)
 p(arr1)
 
-
-for i in arr1:
-    for j in i:
-        circ(j[0]/2, j[1]/2, j[2]/2)
-
-
-window.mainloop()
 
